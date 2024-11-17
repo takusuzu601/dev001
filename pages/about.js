63c144232cea319cd76b6f4e3c70eac6985cb63b
takusuzu601 <takusuzu601@gmail.com>
2024-11-17 12:46:27 +0900

    ✨ アバウトページの追加

---
+++ pages/about.js
@@ -0,0 +1,31 @@
+import Image from "next/image";
+import localFont from "next/font/local";
+import Footer from "../components/Footer";
+import Links from "../components/Links";
+const geistSans = localFont({
+  src: "./fonts/GeistVF.woff",
+  variable: "--font-geist-sans",
+  weight: "100 900",
+});
+const geistMono = localFont({
+  src: "./fonts/GeistMonoVF.woff",
+  variable: "--font-geist-mono",
+  weight: "100 900",
+});
+
+export default function Home() {
+  return (
+
+    <div
+      className={`${geistSans.variable} ${geistMono.variable} grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]`}
+    >
+       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
+        <div className="flex gap-4 items-center flex-col sm:flex-row">
+          <h1 className="text-5xl">About Page</h1>
+        </div>
+      </main>
+      <Links/>
+      <Footer/>
+    </div>
+  );
+}
