e838412b294a1832f240cd90719a7e7dd72b2a07
takusuzu601 <takusuzu601@gmail.com>
2024-11-17 12:45:11 +0900

    ♻️ Footer部分とLinks部分をコンポーネント化

---
+++ components/Footer.js
@@ -0,0 +1,3 @@
+export default function Footer() {
+    return <div>Footer2024.</div>
+  }
---
+++ components/Links.js
@@ -0,0 +1,5 @@
+export default function Links() {
+    return <div>
+        <a href="">Link</a>
+    </div>
+  }
